d6f9167654623927f4943c8bd01dfd881b5a4b25
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2022-11-28 11:58:13 +0100

    Added paramter tests

--- tests/testthat/test-createCharacterizationSettings.R
+++ tests/testthat/test-createCharacterizationSettings.R
@@ -12,9 +12,8 @@ library(TreatmentPatterns)
 library(testthat)
 
 test_that("Void", {
-  expect_s3_class(
-    createCharacterizationSettings(),
-    "characterizationSettings")
+  expect_s3_class(createCharacterizationSettings(),
+                  "characterizationSettings")
 })
 
 test_that("Invalid file - nonexisting file", {
@@ -39,4 +38,58 @@ test_that("Negative minCellCount", {
 test_that("returnCovariates - nonsense", {
   expect_warning(createCharacterizationSettings(returnCovariates = "stuff"),
                  c("using all"))
+})
+
+test_that("baselineCovariates_location", {
+  characterizationSettings <-
+    createCharacterizationSettings(baselineCovariates_location = NULL)
+  
+  expect_null(characterizationSettings$baselineCovariates_location)
+})
+
+test_that("baselineCovariates_location", {
+  expect_error(createCharacterizationSettings(baselineCovariates_location = 3))
+})
+
+test_that("baselineCovariates_location", {
+  expect_error(createCharacterizationSettings(baselineCovariates_location = ""))
+})
+
+test_that("baselineCovariates_location", {
+  expect_s3_class(
+    createCharacterizationSettings(baselineCovariates_location = "./inst/examples/OMOP CDM/inst/settings/characterization_settings.csv"),
+    "characterizationSettings"
+  )
+})
+
+test_that("baselineCovariates_location", {
+  expect_error(
+    createCharacterizationSettings(baselineCovariates_location = "./inst/examples/OMOP CDM/inst/settings/cohorts_to_create.csv")
+  )
+})
+
+test_that("baselineCovariates", {
+  expect_error(createCharacterizationSettings(baselineCovariates = data.frame()))
+})
+
+test_that("baselineCovariates", {
+  expect_error(createCharacterizationSettings(baselineCovariates = list()))
+})
+
+test_that("baselineCovariates", {
+  df <- data.frame(covariateName = c("Age"),
+                   covariateId = c(1002))
+  expect_s3_class(
+    createCharacterizationSettings(baselineCovariates = df),
+    "characterizationSettings"
+  )
+})
+
+test_that("baselineCovariates", {
+  l <- list(covariateName = c("Age"),
+            covariateId = c(1002))
+  expect_s3_class(
+    createCharacterizationSettings(baselineCovariates = l),
+    "characterizationSettings"
+  )
 })

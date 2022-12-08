151251dc809c2aba9cb800ce1fa1d57ccfc2aa1a
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2022-12-08 14:22:45 +0100

    Added execute function that executes a TreatmentPatterns study with createCohortsCG

---
+++ R/darwinExecute.R
@@ -0,0 +1,112 @@
+darwinExecute <-
+  function(targetCohort, cohortPath, dataSettings,
+           pathwaySettings,
+           characterizatonSettings,
+           saveSettings) {
+    # Assuming database is in accordance with the OMOP CDM and that there are
+    # defined cohorts.
+    
+    # Step 1 CohortGenerator
+    createCohortsCG(path = cohortPath,
+                    dataSettings = dataSettings,
+                    type = "JSON")
+    
+    targetCohorts <- cohortTable %>%
+      dplyr::filter(.data$cohortName == targetCohort) %>%
+      dplyr::select(.data$cohortName, .data$cohortId)
+    
+    eventCohorts <- cohortTable %>%
+      dplyr::filter(.data$cohortName != targetCohort) %>%
+      dplyr::select(.data$cohortName, .data$cohortId)
+    
+    # Create cohortSettings
+    cohortSettings <- createCohortSettings(
+      targetCohorts = targetCohorts,
+      eventCohorts = eventCohorts)
+    
+    # Step 2 SaveSettings
+    TreatmentPatterns::cohortCharacterization(
+      dataSettings = dataSettings,
+      characterizationSettings = characterizationSettings,
+      saveSettings = saveSettings)
+    
+    # Step 2
+    TreatmentPatterns::cohortCharacterization(
+      dataSettings = dataSettings,
+      characterizationSettings = characterizationSettings,
+      saveSettings = saveSettings
+    )
+
+    # Step 3
+    TreatmentPatterns::constructPathways(
+      dataSettings = dataSettings,
+      pathwaySettings = pathwaySettings,
+      saveSettings = saveSettings)
+
+    # Step 4
+    TreatmentPatterns::generateOutput(
+      saveSettings = saveSettings)
+    
+    # Step 5
+    TreatmentPatterns::launchResultsExplorer(saveSettings)
+    
+  }
+
+cohortPath <- "D:/Users/mvankessel/Desktop/Viral_sinusitis/JSON BACKUP/"
+
+saveSettings <- createSaveSettings(databaseName = "MyCoolDatabase", rootFolder = ".", outputFolder = "output")
+dataSettings <- createDataSettings(
+  OMOP_CDM = TRUE,
+  connectionDetails = Eunomia::getEunomiaConnectionDetails(),
+  cdmDatabaseSchema = "main",
+  cohortDatabaseSchema = "main",
+  cohortTable = "treatmentpatterns_cohorts"
+)
+characterizationSettings <- createCharacterizationSettings(
+  baselineCovariates = data.frame(
+    covariateName = c('Male', 'Age',
+                      'Charlson comorbidity index score'),
+    covariateId = c(8507001, 1002, 1901)
+  ),
+  returnCovariates = "selection"
+)
+pathwaySettings <- createPathwaySettings(targetCohortId = 7,
+                                         eventCohortIds = c(1, 2, 3, 4, 5, 6))
+
+# cohortSettings <- createCohortSettings(targetCohorts = targetCohorts, eventCohorts = eventCohorts)
+
+createCohortsCG(
+  path = cohortPath,
+  targetCohortName = "Viral Sinusitis",
+  dataSettings = dataSettings,
+  saveSettings = saveSettings,
+  type = "JSON")
+
+TreatmentPatterns::cohortCharacterization(
+  dataSettings = dataSettings,
+  characterizationSettings = characterizationSettings,
+  saveSettings = saveSettings)
+
+TreatmentPatterns::constructPathways(
+  dataSettings = dataSettings,
+  pathwaySettings = pathwaySettings,
+  saveSettings = saveSettings)
+
+TreatmentPatterns::generateOutput(
+  saveSettings = saveSettings)
+
+TreatmentPatterns::launchResultsExplorer(
+  saveSettings = saveSettings)
+
+darwinExecute(cohortPath = "D:/Users/mvankessel/Desktop/Viral_sinusitis/JSON BACKUP/", 
+              dataSettings = dataSettings,
+              cohortSettings = cohortSettings,
+              pathwaySettings = pathwaySettings,
+              characterizatonSettings = characterizationSettings,
+              saveSettings = saveSettings)
+
+connection <- DatabaseConnector::connect(dataSettings$connectionDetails)
+
+DatabaseConnector::renderTranslateQuerySql(connection, "SELECT COUNT(*) FROM treatmentpatterns_cohorts GROUP BY")
+
+

a68f3469c908d02312c38251c9a5e0ff8d9b93f8
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-02-02 09:42:41 +0100

    Added addLabels tests

---
+++ tests/testthat/test-addLabels.R
@@ -0,0 +1,15 @@
+library(TreatmentPatterns)
+library(testthat)
+
+source("inst/examples/R Settings Objects/createDummySettings.R")
+source("tests/testParams.R")
+
+test_that("void", {
+  expect_error(TreatmentPatterns:::addLabels())
+})
+
+test_that("minimal", {
+  expect_s3_class(treatment_history <- TreatmentPatterns:::addLabels(
+    doMaxPathLengthTH, 
+    saveSettings$outputFolder), "data.frame")
+})

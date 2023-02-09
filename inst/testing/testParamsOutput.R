fee6496f91f1686e01c576483c7c7dc145ca9f2f
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-02-09 09:34:14 +0100

    Added script to source fro testing

---
+++ inst/testing/testParamsOutput.R
@@ -0,0 +1,27 @@
+source(list.files(system.file(
+  package = "TreatmentPatterns",
+  "examples", "R Settings Objects"), full.names = TRUE))
+
+TreatmentPatterns::constructPathways(
+  dataSettings = dataSettings,
+  pathwaySettings = pathwaySettings,
+  saveSettings = saveSettings)
+
+# Generate output for folder structure etc.
+generateOutput(saveSettings = saveSettings)
+
+treatment_pathways <- TreatmentPatterns:::getPathways(
+  outputFolder = saveSettings$outputFolder,
+  tempFolder = saveSettings$tempFolder,
+  databaseName = saveSettings$databaseName,
+  studyName = "Viral_Sinusitis",
+  minCellCount = 5)
+
+eventCohortIds <- pathwaySettings$all_settings[3,2]
+eventCohortIds <-
+  unlist(strsplit(eventCohortIds, split = c(";|,")))
+
+groupCombinations <- pathwaySettings$all_settings[16,2]
+
+outputFolder_s <- file.path(saveSettings$outputFolder, "Viral_sinusitis")
+

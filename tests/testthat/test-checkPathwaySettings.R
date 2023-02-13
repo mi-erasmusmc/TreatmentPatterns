db9ff339b4f233ef098fcff160d60534e657215f
CESAR BARBOZA <113351542+cbarbozaerasmus@users.noreply.github.com>
2023-02-13 12:51:30 +0100

    Added test param1

--- tests/testthat/test-checkPathwaySettings.R
+++ tests/testthat/test-checkPathwaySettings.R
@@ -16,6 +16,10 @@ eventCohorts <- data.frame(
   )
 )
 
+cohortSettings <- createCohortSettings(
+  targetCohorts, 
+  eventCohorts)
+
 test_that("Void", {
   expect_error(checkPathwaySettings())
 })
@@ -24,3 +28,31 @@ test_that("Minimal", {
   expect_equal(checkPathwaySettings(cohortSettings),
                TRUE)
 })
+
+#optionalParam1
+
+targetCohorts <- data.frame(cohortId = c(1),
+                            cohortName = c('Hypertension'))
+
+eventCohorts <- data.frame(
+  cohortId = c("JAMES", 11, 12, 13, 14),
+  cohortName = c(
+    'Hydrochlorothiazide',
+    'Metorolol',
+    'Amlodipine',
+    'Lisinopril',
+    'Losartan'
+  )
+)
+
+cohortSettings <- createCohortSettings(
+  targetCohorts, 
+  eventCohorts)
+
+test_that("optionalParam1", {
+  expect_error(checkPathwaySettings(cohortSettings))
+})
+
+
+
+

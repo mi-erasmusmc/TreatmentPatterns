1bcb3db98305d7a5c5ac15edf3fd603b8f54f2a5
Ger Inberg <ginberg@gmail.com>
2023-02-14 15:48:47 +0100

    update test

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -56,23 +56,30 @@ loadRenderTranslateSql <- function(
 #' 
 #' Extract table with specific name from database server.
 #'
-#' @param connection
-#'     Connection object
-#' @param tableName
-#'     Name of table
-#' @param resultsSchema
-#'     Schema of results
+#' @param connection DatabaseConnector connection object
+#' @param tableName Name of table
+#' @param resultsSchema Schema of results
 #' @param dbms Name of dbms to use
 #'
+#' @import checkmate
+#' @import DatabaseConnector
+#' @import SqlRender
+#' 
 #' @return data the extracted table as a data.frame
 #' @export
 #' 
-#' #' @examples
+#' @examples
 #' \dontrun{
 #'   con <- DatabaseConnector::connect(Eunomia::getEunomiaConnectionDetails())
 #'   extractFile(con, "person", "main", "sqlite")
 #'}
 extractFile <- function(connection, tableName, resultsSchema, dbms) {
+  # Assertions
+  checkmate::checkClass(connection, "DatabaseConnectorDbiConnection")
+  checkmate::checkCharacter(tableName, len = 1)
+  checkmate::checkCharacter(resultsSchema, len = 1)
+  checkmate::checkCharacter(dbms, len = 1)
+  
   parameterizedSql <- "SELECT * FROM @resultsSchema.@tableName"
   renderedSql <- SqlRender::render(
     parameterizedSql,

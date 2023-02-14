eceffbc967d50780ca5c339cce3ce63c956efb32
Ger Inberg <ginberg@gmail.com>
2023-02-14 13:43:34 +0100

    add doc, checks

--- R/saveAsFile.R
+++ R/saveAsFile.R
@@ -1,4 +1,6 @@
 #' saveAsFile
+#' 
+#' Save an SVG-image from a html-file as a PNG, - or PDF-file.
 #'
 #' @param fileName HTML-Filename.
 #' @param fileNameOut Filename of image, either with .pdf or .png extension.
@@ -6,15 +8,22 @@
 #' @param vwidth Width of frame to capture, default = 430.
 #' @param ... Other potential parameters to be passed to webshot2::webshot()
 #'
+#' @import checkmate
 #' @import webshot2
 #'
-#' @return NULL
+#' @return Invisibly returns the normalized path to all screenshots taken. The character vector will have a class of '"webshot"'.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #'   saveAsFile("webFile.html", "outputFile.png")}
 saveAsFile <- function(fileName, fileNameOut, zoom = 3, vwidth = 430, ...) {
+  # Assertions
+  checkmate::assertCharacter(x = fileName, pattern = "*.html", null.ok = FALSE)
+  checkmate::assertCharacter(x = fileNameOut, pattern = "*.pdf|*.png", null.ok = FALSE)
+  checkmate::assertNumeric(x = zoom, lower = 0, len = 1, null.ok = FALSE)
+  checkmate::assertNumeric(x = vwidth, lower = 0, len = 1, null.ok = FALSE)
+  
   webshot2::webshot(
     url = fileName,
     file = fileNameOut,

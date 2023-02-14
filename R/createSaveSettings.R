d1694ff50b4042cc2690761c6696a3964fa79285
CESAR BARBOZA <113351542+cbarbozaerasmus@users.noreply.github.com>
2023-02-14 11:49:20 +0100

    Lintr review

--- R/createSaveSettings.R
+++ R/createSaveSettings.R
@@ -1,12 +1,11 @@
 #' checkSaveSettings
-#'
 #' Checks parameters for createSaveSettings.
 #'
-#' @param databaseName 
-#' @param rootFolder 
-#' @param outputFolder 
-#' @param tempFolder 
-#' 
+#' @param databaseName
+#' @param rootFolder
+#' @param outputFolder
+#' @param tempFolder
+#'
 #' @import checkmate
 #'
 #' @return TRUE if all assertions pass
@@ -21,18 +20,18 @@ checkSaveSettings <- function(
     x = databaseName,
     len = 1,
     any.missing = FALSE))
-  
+
   # rootFolder
   checkmate::assert(checkmate::checkDirectory(
     x = rootFolder,
     access = "wx"))
-  
+
   if (!is.null(outputFolder)) {
     # outputFolder
     checkmate::assert(checkmate::check_character(
       outputFolder))
   }
-  
+
   if (!is.null(tempFolder)) {
     # tempFolder
     checkmate::assert(checkmate::checkPathForOutput(
@@ -42,52 +41,54 @@ checkSaveSettings <- function(
 }
 
 #' createSaveSettings
-#' 
+#'
 #' Creates saveSettings object.
 #'
 #' @param databaseName
 #'     Name of the database that will appear in the results.
-#'     
+#'
 #' @param rootFolder
 #'     Name of local folder to place all package output (outputFolder,
 #'     tempFolder if not given).
 #'
 #' @return
 #'     Object saveSettings.
-#'     
+#'
 #' @export
 createSaveSettings <- function(
     databaseName = "unknown_name",
     rootFolder,
     outputFolder = NULL,
     tempFolder = NULL) {
-  
+
   if (is.null(outputFolder)) {
     outputFolder <- file.path(rootFolder, "output")
   }
-  
+
   if (is.null(tempFolder)) {
     tempFolder <- file.path(rootFolder, "temp")
   }
-      
+
   check <- checkSaveSettings(
     databaseName,
     rootFolder,
     outputFolder,
     tempFolder)
-  
+
   if (check) {
     # Change relative path to absolute path
     rootFolder <- normalizePath(rootFolder)
-    
+
     saveSettings <- list(
       databaseName = databaseName,
       rootFolder = rootFolder,
       outputFolder = outputFolder,
       tempFolder = tempFolder)
-    
-    class(saveSettings) <- 'saveSettings'
-    
+
+    class(saveSettings) <- "saveSettings"
+
     return(saveSettings)
+
   }
+
 }

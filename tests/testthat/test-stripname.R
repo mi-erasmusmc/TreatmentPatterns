c61375391a7463e035ca5fedffe7237111ac3290
Ger Inberg <ginberg@gmail.com>
2023-02-15 12:59:41 +0100

    update test

--- tests/testthat/test-stripname.R
+++ tests/testthat/test-stripname.R
@@ -1,27 +1,24 @@
 library(testthat)
 library(TreatmentPatterns)
 
+simpleList <- list(
+  one = 1,
+  two = 2,
+  three = 3)
+
+nestedList <- c(simpleList, nest = list(
+  None = 1,
+  Ntwo = 2,
+  Nthree = 3))
+
 test_that("void", {
   expect_error(TreatmentPatterns:::stripname())
 })
 
 test_that("minimal", {
-  nestedList <- list(
-    one = 1,
-    two = 2,
-    three = 3,
-    nest = list(
-      None = 1,
-      Ntwo = 2,
-      Nthree = 3))
-  
   out <- TreatmentPatterns:::stripname(x = nestedList, name = "nest")
   expect_null(names(out$nest))
   
-  simpleList <- list(
-    one = 1,
-    two = 2,
-    three = 3)
   out <- TreatmentPatterns:::stripname(x = simpleList, name = "nest")
   expect_equal(out, simpleList)
 })

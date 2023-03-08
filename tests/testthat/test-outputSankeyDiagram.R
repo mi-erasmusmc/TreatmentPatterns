912041243ae213b02cdc0d6a4fc6cff39a24ec58
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2023-03-08 10:51:57 +0100

    Added code as comments to test functionality

--- tests/testthat/test-outputSankeyDiagram.R
+++ tests/testthat/test-outputSankeyDiagram.R
@@ -20,3 +20,26 @@ test_that("Minimal", {
     "Viral_Sinusitis",
     paste0("Eunomia", "_", "Viral_Sinusitis", "_all_sankeydiagram.html"))))
 })
+
+# data <- TreatmentPatterns:::groupInfrequentCombinations(
+#   data = treatment_pathways[[1]], groupCombinations = TRUE)
+# 
+# data[is.na(data)] <- "Stopped"
+# 
+# data$event_cohort_name1 <- paste0("1. ", data$event_cohort_name1)
+# data$event_cohort_name2 <- paste0("2. ", data$event_cohort_name2)
+# data$event_cohort_name3 <- paste0("3. ", data$event_cohort_name3)
+# 
+# results1 <- data %>%
+#   dplyr::group_by(event_cohort_name1, event_cohort_name2) %>%
+#   dplyr::summarise(freq = sum(freq))
+# 
+# data
+# 
+# data %>%
+#   dplyr::group_by(event_cohort_name2, event_cohort_name3) %>%
+#   dplyr::summarise(freq = sum(freq))
+# 
+# colnames(results1) <- c("source", "target", "value")
+# colnames(results2) <- c("source", "target", "value")
+# links <- as.data.frame(rbind(results1, results2))
